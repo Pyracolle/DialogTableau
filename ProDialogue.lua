73bd651532ac24d2a989b91dc67397610e5420f6
Pyracolle <147287149+Pyracolle@users.noreply.github.com>
2024-02-06 17:11:36 -0500

    Update ProDialogue.lua

--- ProDialogue.lua
+++ ProDialogue.lua
@@ -5,55 +5,55 @@ local RunService = game:GetService("RunService")
 --------------------------------- PRO - DIALOGUE ------------------------------------
 -- Writen by @mari230899
 -- Created: 25/10/2022
--- Updated: 16/08/2023
+-- Updated: 06/02/2024
 
--- https://devforum.roblox.com/t/2033941
+-- https://devforum.roblox.com/t/2033941 (outdated info)
 -- Thanks for using the module!
 -------------------------------------------------------------------------------------
+--]]
 
--- A template would look like this
--- NORMAL CHAT --------------------------------
-                                                               --[[
+-- CLASSIC FORMAT --
+--[[
+local config = {
+	Body = {                    -- only this table is necessary
+		"Hi adventurer",
+		"I hope you are good!"
+	};
+	AllowMovement = false;
+	canSkip = false;
+}
+
 local ProDialogue = require(game.ReplicatedStorage.ProDialogue)
-local player = game.Players.LocalPlayer
+local constructor = ProDialogue.new(config)
 
-local dialogue = ProDialogue.new(player, {
-	Body = {
-		"Message1",
-		"Message2",
-	};
-	typeSpeed = 0.08; -- // speed at which letters will appear in dialogue \\
-	BetweenChat = 0.9; -- // waiting time between dialogues \\
-	canSkip = false; -- // Player power to skip dialogues with a click \\
-	AllowMovement = false -- // Player power to move in dialogue \\
-})
-
-dialogue:Begin("NPC") -- // name of the speaker
-dialogue._Completed.Event:Event()
-                                                               --]]
-
-
-
--- If you want to create options, create the variable 'chatChoice'
--- chatChoice: table, values on table: string
--- CHOICE CHAT --------------------------------
-                                                               --[[
-local dialogue = ProDialogue.new(player, {
-	Body = {
-		"Message1",
-		"Message2",
-	};
-	chatChoice = {"response1, response2"}
-})
-
-dialogue:Begin("NPC")
-
-dialogue._Completed.Event:Connect(function(val)
-	if val == "Message2" then
-		print("player chose the second option")
-	end
+script.Parent.ProximityPrompt.Triggered:Connect(function(player)
+	constructor:Begin(player, "Guideman") -- u can add an optional image
 end)
-                                                               --]]
+--]]
+
+-- CHOICE FORMAT --
+--[[
+local config = {
+	Body = {
+		"Hi adventurer",
+		"How are you?"
+	};
+	AllowMovement = false;
+	chatChoice = {"GOOD", "Im sad :("};
+}
+
+local ProDialogue = require(game.ReplicatedStorage.ProDialogue)
+local constructor = ProDialogue.new(config)
+
+script.Parent.ProximityPrompt.Triggered:Connect(function(player)
+	constructor:Begin(player, "Guideman")
+	constructor._Completed.Event:Connect(function(answer)
+		if answer == "GOOD" then
+			-- create another dialogue or smt
+		end
+	end)
+end)
+--]]
 
 local dialogue = {}
 dialogue .__index = dialogue 
@@ -63,41 +63,22 @@ local PureFrame = require(script.PureFrame)       -- Custom Roact frame maker
 local IntError = require(script.IntError)         -- Auxiliar error checker
 local onqueue = false                             -- Already using dialogue system
 
-function typewrite(label: TextLabel, txt: string, wt: number, parent: Model)
-	for i = 1, #txt, 1 do
-		label.Text = string.sub(txt, 1, i)
-		parent.Click:Play()
-		task.wait(wt)
-	end
-end
-
--- Custom type, makes the configuration of the module more friendly
 export type DialogueConfig = {
-	Body: table,
-	typeSpeed: number,
-	BetweenChat: number,
-	chatChoice: table | nil,
+	Body: table,                                  -- Main dialogue table
+	typeSpeed: number,                            -- Delay between every letter
+	BetweenChat: number,                          -- Delay between every text
+	chatChoice: table | nil,                      -- Classic and Options
 	
-	canSkip: boolean,
-	AllowMovement: boolean,
+	canSkip: boolean,                             -- A button to skip dialogue will appear
+	AllowMovement: boolean,                       -- Character can move in dialogue
 }
 
 -- Main ProDialogue constructor
-function dialogue.new(player: Player, configuration: DialogueConfig)
-	assert(player.Character, "[ProDialogue]: Loaded too fast | CharacterModel equals nil")
-	assert(player.PlayerGui, "[ProDialogue]: Loaded too fast | PlayerGui equals nil")
-	
-	for index, stringopt in configuration.Body do
-		IntError(stringopt, "string")
+function dialogue.new(configuration: DialogueConfig)
+	for index, corpse in configuration.Body do
+		IntError(corpse, "string")
 	end
-	
 	local self = setmetatable({}, dialogue)
-	
-	-- SPECIAL --
-	self._player = player
-	self._PlayerGui = player:FindFirstChild("PlayerGui")
-	self._Character = player.Character
-	self._connection = nil
 	
 	-- BOOLEAN --
 	self._IsActive = false
@@ -112,24 +93,32 @@ function dialogue.new(player: Player, configuration: DialogueConfig)
 	self._config.BetweenChat = configuration.BetweenChat or 0.9
 	self._config.canSkip = configuration.canSkip or false
 	self._config.AllowMovement = configuration.AllowMovement or false
-	self._config.chatChoice = configuration.chatChoice
+	self._config.chatChoice = configuration.chatChoice or nil
 	
 	return self
 end
 
-function dialogue:Begin(actor: string, imageIcon: string)
-	assert(self._Character, "[ProDialogue]: Call the function from the constructor")
+function dialogue:Begin(player: Player, actor: string, imageIcon: string)
+	assert(self._config, "[ProDialogue]: Call the function from the constructor")
+	IntError(player, "Player")
+	IntError(actor, "string")
 	
-	local Humanoid = self._Character:WaitForChild("Humanoid")
+	local Character = player.Character
+	local PlayerGui = player:WaitForChild("PlayerGui")
+	local Humanoid = Character:WaitForChild("Humanoid")
 	local WalkSpeed = Humanoid.WalkSpeed
 	local JumpPower = Humanoid.JumpPower
 	local UI = nil
 	local SelectedOption = nil
+	local allowIcon = false
 	local skipinst = false
 	
+	if typeof(imageIcon) == "string" then
+		allowIcon = true
+	end
+	
 	if onqueue == false then
-		if self._PlayerGui ~= nil then
-			IntError(actor, "string")
+		if PlayerGui ~= nil then
 			onqueue = true
 			self._IsActive = true
 			
@@ -140,14 +129,14 @@ function dialogue:Begin(actor: string, imageIcon: string)
 			end
 			
 			-- // Create dialogue UI \\ --
-			if typeof(imageIcon) ~= "string" then
-				UI = PureFrame["dialogue_Model1"]()
+			if allowIcon then
+				UI = PureFrame["dialogue_1_icon"]()
 			else
-				UI = PureFrame["dialogue_Model2"]()
+				UI = PureFrame["dialogue_1"]()
 			end
 			
-			Roact.mount(UI, self._PlayerGui)
-			local Interface = self._PlayerGui:WaitForChild("ProDialogue")
+			Roact.mount(UI, PlayerGui)
+			local Interface = PlayerGui:WaitForChild("ProDialogue")
 			Interface.Main.Title.Text = actor
 			
 			pcall(function()
@@ -161,26 +150,19 @@ function dialogue:Begin(actor: string, imageIcon: string)
 			audio.Volume = 2
 			audio.PlaybackSpeed = 1.8
 			audio.RollOffMaxDistance = 15
-			audio.Parent = self._Character
+			audio.Parent = Character
 			
-			self._connection = UserInputService.InputBegan:Connect(function(input, gp)
-				if self._config.canSkip then
-					if not gp and input.UserInputType == Enum.UserInputType.MouseButton1 then
-						skipinst = true
-					end
-				end
-			end)
+			if self._config.canSkip then
+				Interface.Main.skip.Visible = true
+				Interface.Main.skip.MouseButton1Click:Connect(function()
+					skipinst = true
+				end)
+			end
 			
 			local function typewrite(text: string)
 				for i = 1, #text, 1 do
-					self._Character.Click:Play()
-					
-					if self._IsActive == false then
-						repeat task.wait()
-							Interface.Main.Description.Text = text
-						until self._IsActive
-						break
-					end
+					Character.Click:Play()
+					if self._IsActive == false then break end
 					
 					if skipinst then
 						Interface.Main.Description.Text = text
@@ -195,63 +177,58 @@ function dialogue:Begin(actor: string, imageIcon: string)
 			-- // Actual dialogue system \\ --
 			coroutine.resume(coroutine.create(function()
 				for index, txt in ipairs(self._config.Body) do
+					if self._IsActive == false then break end
 					TweenService:Create(
 						Interface.Main, TweenInfo.new(
 							0.2, Enum.EasingStyle.Linear, Enum.EasingDirection.In, 0, true
 						),
 						{Position = Interface.Main.Position - UDim2.new(0, 0, 0.01, 0)}
 					):Play()
-					
+
 					typewrite(txt)
 					task.wait(self._config.BetweenChat)
 				end
 				Interface:Destroy()
 				
-				if self._config.chatChoice then
-					local ChoiceChatUI = PureFrame["dialogue_Model3"]()
-					Roact.mount(ChoiceChatUI, self._PlayerGui)
+				if self._config.chatChoice ~= nil and self._IsActive then
+					IntError(self._config.chatChoice, "table")
 					
-					Interface = self._PlayerGui:WaitForChild("ProDialogue")
-					Interface.Main.Title.Text = actor
-					Interface.Main.Description.Text = self._config.Body[#self._config.Body]
-					
-					for index, choice in pairs(self._config.chatChoice) do
-						IntError(choice, "string")
-						
-						local btn = Instance.new("TextButton")
-						local Id = "rbxasset://fonts/families/ComicNeueAngular.json"
-						btn.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-						btn.BackgroundTransparency = 0.6
-						btn.FontFace = Font.new(Id, Enum.FontWeight.Regular)
-						btn.TextColor3 = Color3.fromRGB(255, 255, 255)
-						btn.Size = UDim2.new(1, 0, 0, 44)
-						btn.TextSize = 20
-						btn.Name = choice
-						btn.Text = choice
-						btn.Parent = Interface.Options
-					end
-					
-					repeat RunService.Heartbeat:Wait()
-						for index, choice in pairs(Interface.Options:GetChildren()) do
-							if choice:IsA("TextButton") then
-								choice.MouseButton1Click:Connect(function()
-									SelectedOption = choice.Text
-								end)
-							end
+					if #self._config.chatChoice ~= 0 and #self._config.chatChoice < 4 then
+						local ChoiceUI
+						if allowIcon then
+							ChoiceUI = PureFrame["dialogue_2_icon"]()
+						else
+							ChoiceUI = PureFrame["dialogue_2"]()
 						end
-					until SelectedOption ~= nil
-					Interface:Destroy()
+						
+						Roact.mount(ChoiceUI, PlayerGui)
+						Interface = PlayerGui:WaitForChild("ProDialogue")
+						Interface.Main.Title.Text = actor
+						typewrite(self._config.Body[#self._config.Body])
+						
+						pcall(function()
+							Interface.Main.Icon.Image = imageIcon
+						end)
+						
+						for index, txt in pairs(self._config.chatChoice) do
+							local btn = Interface.Main.Options["Opt"..tostring(index)]
+							btn.Visible = true
+							btn.Text = txt
+							
+							btn.MouseButton1Click:Connect(function()
+								SelectedOption = txt
+							end)
+						end
+						repeat RunService.Heartbeat:Wait() until SelectedOption ~= nil
+						Interface:Destroy()
+					end
 				end
 				onqueue = false
 				skipinst = false
 				
-				if self._connection then
-					self._connection:Disconnect()
-				end
-				
 				Humanoid.WalkSpeed = WalkSpeed
 				Humanoid.JumpPower = JumpPower
-				self._Character.Click:Destroy()
+				Character.Click:Destroy()
 				self._Completed:Fire(SelectedOption)
 			end))
 		else
@@ -260,28 +237,11 @@ function dialogue:Begin(actor: string, imageIcon: string)
 	end
 end
 
-function dialogue:Pause(timer: number)
-	assert(self._Character, "[ProDialogue]: Call the function from the constructor")
+function dialogue:Destroy()
+	assert(self._config, "[ProDialogue]: Call the function from the constructor")
 	
 	if onqueue == true then
 		self._IsActive = false
-		
-		if timer ~= nil then
-			IntError(timer, "number")
-			
-			coroutine.resume(coroutine.create(function()
-				task.wait(timer)
-				self._IsActive = true
-			end))
-		end
-	end
-end
-
-function dialogue:Resume()
-	assert(self._Character, "[ProDialogue]: Call the function from the constructor")
-	
-	if onqueue == true and self._IsActive == false then
-		self._IsActive = true
 	end
 end
 
